dfdc0cde1f0f413c3ee2173dfe71ec4059e6e4d4
Jasmine Tran <jasetran@uci.edu>
2024-10-05 12:46:54 -0700

    milky way theme files

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,4 @@
 import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-
-import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
-export default defineConfig({
-	site: 'https://jasetran.github.io',
-	integrations: [mdx(), sitemap()],
-});
+export default defineConfig({});
